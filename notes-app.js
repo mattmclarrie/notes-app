a59bda95e26bf26869aef0b2c2286e348201afc5
Matt McLarrie <matthewmclarrie@Matts-MacBook-Air.local>
2018-11-20 13:00:41 +0000

    First upload with name change

---
+++ notes-app.js
@@ -0,0 +1,26 @@
+const notes = getSavedNotes()
+
+const filters = {
+    searchText: ''
+}
+
+renderNotes(notes, filters)
+
+document.querySelector('#create-note').addEventListener('click', function (event) {
+    notes.push({
+        id: uuidv4(),
+        title: '',
+        body: ''
+    })
+    savedNotes(notes)
+    renderNotes(notes, filters)
+})
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
+
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
+})
---
+++ notes-functions.js
@@ -0,0 +1,51 @@
+// Read existing notes from local storage
+const getSavedNotes = function () {
+    const notesJSON = localStorage.getItem('notes')
+
+    if (notesJSON !== null) {
+        return JSON.parse(notesJSON)
+    } else {
+        return []
+    }
+}
+
+// Save the notes to local storage
+const savedNotes = function (notes) {
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+// Generate the DOM structure for a note
+const generateNoteDOM = function (note) {
+    const noteEl = document.createElement('div')
+    const textEl = document.createElement('span')
+    const button = document.createElement('button')
+
+    // Setup remove note button
+    button.textContent = 'x'
+    noteEl.appendChild(button)
+
+    // Setup the note title text
+    if (note.title.length > 0) {
+        textEl.textContent = note.title
+    } else {
+        textEl.textContent = 'Unnamed note'
+    }
+
+    noteEl.appendChild(textEl)
+
+    return noteEl
+}
+
+// Render application notes
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach(function (note) {
+        const noteEl = generateNoteDOM(note)
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
