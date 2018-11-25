1340ae283a0193c95007b974bb6e6313da0f98a4
Matt McLarrie <matthewmclarrie@Matts-MacBook-Air.local>
2018-11-25 21:57:57 +0000

    Remove note button added

--- notes-edit.js
+++ notes-edit.js
@@ -1,3 +1,6 @@
+const titleElement = document.querySelector('#note-title')
+const bodyElement = document.querySelector('#note-body')
+const removeElement = document.querySelector('#remove-note')
 const noteId = location.hash.substring(1)
 const notes = getSavedNotes()
 const note = notes.find(function (note) {
@@ -8,6 +11,21 @@ if (note === undefined) {
     location.assign('/index.html')
 }
 
-document.querySelector('#note-title').value = note.title
-document.querySelector('note-body').value - note.body
-document.querySelector('#remove-note').addEventListener('click', removeNote())
+titleElement.value = note.title
+bodyElement.value = note.body
+
+titleElement.addEventListener('input', function (e) {
+    note.title = e.target.value
+    saveNotes(notes)
+})
+
+bodyElement.addEventListener('input', function (e) {
+    note.body = e.target.value
+    saveNotes(notes)
+})
+
+removeElement.addEventListener('click', function (e) {
+    removeNote(note.id)
+    saveNotes(notes)
+    location.assign('/index.html')
+})
