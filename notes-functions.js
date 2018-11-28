027fff814df293422c963eefb6255d03a54f86c9
Matt McLarrie <mmclarrie@gmail.com>
2018-11-28 08:46:33 +0000

    Sync between notes added

--- notes-functions.js
+++ notes-functions.js
@@ -1,12 +1,7 @@
 // Read existing notes from localStorage
 const getSavedNotes = () => {
     const notesJSON = localStorage.getItem('notes')
-
-    if (notesJSON !== null) {
-        return JSON.parse(notesJSON)
-    } else {
-        return []
-    }
+    return notesJSON ? JSON.parse(notesJSON) : []
 }
 
 // Save the notes to localStorage
