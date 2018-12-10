c664e483cb7e73b66fae9b95b73c7ddea4fd6150
Matt McLarrie <mmclarrie@gmail.com>
2018-12-10 19:19:23 +0000

    Use strict mode

--- notes-edit.js
+++ notes-edit.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const titleElement = document.querySelector('#note-title')
 const lastEdited = document.querySelector('#last-edited')
 const bodyElement = document.querySelector('#note-body')
